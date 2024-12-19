caaa7eaa7b6aeb62b26d4e3a5ce5842fca33774a
Shivendra Upadhyay <shivendrau@leewayhertz.com>
2024-12-19 18:01:37 +0530

    added typechecking multiply fn

--- mutiply.js
+++ mutiply.js
@@ -1,4 +1,7 @@
 function multiply(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    throw new Error("Non numbers not supported");
+  }
   let num1 = a;
   let num2 = b; // this is a test comment for dev branch
   return num1 * num2;
