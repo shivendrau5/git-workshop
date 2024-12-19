e6e61a56f655930bc03660b5d9aea5681ad7b4ea
Shivendra Upadhyay <shivendrau@leewayhertz.com>
2024-12-19 17:30:07 +0530

    added comment for new-feature branch

--- mutiply.js
+++ mutiply.js
@@ -1,6 +1,6 @@
 function multiply(a, b) {
   let num1 = a;
-  let num2 = b; // this is a test comment for development branch
+  let num2 = b; // this is a test comment for new-feature branch
   return num1 * num2;
 }
 let ans = multiply(10, 20);
